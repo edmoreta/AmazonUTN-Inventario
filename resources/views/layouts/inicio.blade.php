ed5ab978993f3f6714a580f10fae414c00662af7
AlexanderPatricio <alexanderpatricio@outlook.es>
2019-02-17 13:15:49 -0500

    Formato, y restablecer contraseña usuario

--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -87,7 +87,6 @@
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
           <!-- Sidebar user panel -->
-                    
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
         
