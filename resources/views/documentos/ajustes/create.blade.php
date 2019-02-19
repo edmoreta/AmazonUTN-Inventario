42572e317786e7d69040dce0f0abab7fe5ebe6fb
MrSoundMaurix <reneipiales1200@gmail.com>
2019-02-19 13:18:42 -0500

    $ agregados

--- resources/views/documentos/ajustes/create.blade.php
+++ resources/views/documentos/ajustes/create.blade.php
@@ -3,125 +3,123 @@
 <div class="card-header"> <a class="btn btn-success" href="{{url('documentos')}}" title="Regresar al listado" role="button">
 		<i class="fa fa-reply" aria-hidden="true"></i>
 	</a></div>
- <div class="row">
+<div class="row">
     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-			@include('includes.messages')
-	</div>
-	<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+    @include('includes.messages')
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
         <h3>Nuevo Ajuste</h3>
     </div>
- </div>
+</div>
 
- {!!Form::open(array('url'=>'ajustes','method'=>'POST','autocomplete'=>'off'))!!}
- {{Form::token()}}
+{!!Form::open(array('url'=>'ajustes','method'=>'POST','autocomplete'=>'off'))!!} {{Form::token()}}
 <div class="row">
-	<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
 
-	 </div>
-	 <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+    </div>
+    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
 
-	 </div>
-	 <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-			   <div class="form-group">
-					  <label for="serieComprobante">Código De Ajuste</label>
-					  <input type="text" name="doc_codig" id="doc_codig" disabled value="AJ-{{$codigo}}"  required value="{{old('doc_codigo')}}" class="form-control" placeholder="Serie del Comprobante..">
-			   </div>
-	 </div>
-	 <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
-			   <div class="form-group">
-					 <label for="doc_fecha">Fecha De Ajuste</label>
-					 <input type="date" name="doc_fecha" id="doc_fecha" required value="{{$fecha_actual}}" max="{{$fecha_actual}}" class="form-control" placeholder="Numero del Comprobante..">
-			   </div>
-	 </div>
+    </div>
+    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+        <div class="form-group">
+            <label for="serieComprobante">Código De Ajuste</label>
+            <input type="text" name="doc_codig" id="doc_codig" disabled value="AJ-{{$codigo}}" required value="{{old('doc_codigo')}}"
+                class="form-control" placeholder="Serie del Comprobante..">
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+        <div class="form-group">
+            <label for="doc_fecha">Fecha De Ajuste</label>
+            <input type="date" name="doc_fecha" id="doc_fecha" required value="{{$fecha_actual}}" max="{{$fecha_actual}}" class="form-control"
+                placeholder="Numero del Comprobante..">
+        </div>
+    </div>
 </div>
 <div class="row">
-	 <div class="panel panel-primary">
-			   <div class="panel-body">
-						<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-								<div class="form-group">
-									<label>Producto</label>
-									<select name="ppro_id" id="ppro_id" class="form-control selectpicker" data-live-search="true">
+    <div class="panel panel-primary">
+        <div class="panel-body">
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <label>Producto</label>
+                    <select name="ppro_id" id="ppro_id" class="form-control selectpicker" data-live-search="true">
 										@foreach($productos as $prod)
 										<option value="{{$prod->pro_id}}_{{$prod->pro_stock}}_{{$prod->pro_costo}}_{{$prod->pro_precio}}">{{$prod->pro_nombre}}</option>
 										@endforeach
 									</select>
-								 </div>
-						</div>
-						<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-								  <div class="form-group">
-									  <label for="pcantidad">Cantidad</label>
-									  <input type="number" name="pcantidad" id="pcantidad" class="form-control" placeholder="Cantidad">
-								  </div>
-						</div>
-						<input type="hidden" name="doc_codigo" id="doc_codigo" value="AJ-{{$codigo}}"  required value="{{old('doc_codigo')}}" class="form-control">
-						<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-								  <div class="form-group">
-									  <label for="stock">Stock</label>
-									  <input type="number" disabled name="pstock" id="pstock" class="form-control" placeholder="Stock..">
-								  </div>
-					   </div>
-					   <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-							<div class="form-group">
-								  <label for="pcosto">Costo</label>
-								  <input type="number"  disabled name="pcosto" id="pcosto" class="form-control" placeholder="Costo..">
-							</div>
-					 </div>
-					   <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-								  <div class="form-group">
-										<label for="pprecio">Precio</label>
-										<input type="number"   name="pprecio" id="pprecio" class="form-control" placeholder="Precio..">
-								  </div>
-					   </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <label for="pcantidad">Cantidad</label>
+                    <input type="number" name="pcantidad" id="pcantidad" class="form-control" placeholder="Cantidad">
+                </div>
+            </div>
+            <input type="hidden" name="doc_codigo" id="doc_codigo" value="AJ-{{$codigo}}" required value="{{old('doc_codigo')}}" class="form-control">
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <label for="stock">Stock</label>
+                    <input type="number" disabled name="pstock" id="pstock" class="form-control" placeholder="Stock..">
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <label for="pcosto">Costo</label>
+                    <input type="number" disabled name="pcosto" id="pcosto" class="form-control" placeholder="Costo..">
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <label for="pprecio">Precio</label>
+                    <input type="number" name="pprecio" id="pprecio" class="form-control" placeholder="Precio..">
+                </div>
+            </div>
 
-					   <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-								  <div class="form-group">
-									  <label for="ptipo_ajuste">Tipo Ajuste</label><br>
-									  <select name="ptipo_ajuste" id="ptipo_ajuste" class="form-control selectpicker" data-live-search="true">
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <label for="ptipo_ajuste">Tipo Ajuste</label><br>
+                    <select name="ptipo_ajuste" id="ptipo_ajuste" class="form-control selectpicker" data-live-search="true">
 											<option value="Positivo">Positivo</option>
 											<option value="Negativo">Negativo</option>
 									</select>
-								  </div>
-					   </div>
-					   <div class="col-lg-10 col-sm-10 col-md-10 col-xs-12">
-						</div>
-					   <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
-								   <div class="form-group">
-										<button class="btn btn-danger" type="button" id="bt_add">Agregar</button>
-								   </div>
-					   </div>
-					   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-								 <div class="table-responsive">
-										 <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
-											  <thead style="background-color: #dd4b39">
-												   <th>Opciones</th>
-												   <th>Producto</th>
-												   <th>Cantidad</th>
-												   <th>Stock</th>
-												   <th>Costo</th>
-												   <th>Precio</th>
-												   <th>Tipo Ajuste</th>
-											 </thead>
-											  <tbody>
-											  </tbody>
-										  </table>
-								   </div>
-						 </div>
-		</div>
-  </div>
-  <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" id="guardar">
-		  <div class="form-group">
-				   <input name="_token" value="{{ csrf_token() }}" type="hidden"></input>
-				   <button class="btn btn-danger" type="reset">Cancelar</button>
-				   <button class="btn btn-primary" type="submit">Guardar</button>
-		  </div>
-  </div>
+                </div>
+            </div>
+            <div class="col-lg-10 col-sm-10 col-md-10 col-xs-12">
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+                <div class="form-group">
+                    <button class="btn btn-danger" type="button" id="bt_add">Agregar</button>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="table-responsive">
+                    <table id="detalles" class="table table-striped table-bordered table-condensed table-hover">
+                        <thead style="background-color: #dd4b39">
+                            <th>Opciones</th>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Stock</th>
+                            <th>Costo</th>
+                            <th>Precio</th>
+                            <th>Tipo Ajuste</th>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12" id="guardar">
+        <div class="form-group">
+            <input name="_token" value="{{ csrf_token() }}" type="hidden"></input>
+            <button class="btn btn-danger" type="reset">Cancelar</button>
+            <button class="btn btn-primary" type="submit">Guardar</button>
+        </div>
+    </div>
 </div>
-{!!Form::close()!!}
-
-
-@push ('scripts')
+{!!Form::close()!!} @push ('scripts')
 <script>
-	$(document).ready(function(){
+    $(document).ready(function(){
 		$('#bt_add').click(function(){
 	   agregar();
 	   });
@@ -208,7 +206,9 @@ function evaluar()
   evaluar();
 
 }
+
 </script>
+
 
 
 @endpush
--- resources/views/documentos/stock.blade.php
+++ resources/views/documentos/stock.blade.php
@@ -1,86 +1,90 @@
 @extends ('layouts.inicio')
 @section ('contenido')
 <div class="row">
-	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">		
-	</div>
-	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-		<center><h2>Movimientos</h2></center> 
-		@include('documentos.search2')		
-	</div>
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <center>
+            <h2>Movimientos</h2>
+        </center>
+    @include('documentos.search2')
+    </div>
 </div>
 <div class="row">
-		<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
-				@include('includes.messages')
-		</div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+    @include('includes.messages')
+    </div>
 </div>
 <div class="row">
-	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		<div class="table-responsive">
-			<table class="table table-striped table-bordered table-condensed table-hover">
-				<thead>
-					<th >Tipo</th>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-condensed table-hover">
+                <thead>
+                    <th>Tipo</th>
                     <th>Codigo</th>
                     <th>Fecha</th>
-					<th>Producto</th>
-					<th>Cantidad</th>
+                    <th>Producto</th>
+                    <th>Cantidad</th>
                     <th>Precio</th>
                     <th>Costo</th>
-                   
-				</thead>
-               @foreach ($movimientos as $mov)
-				<tr>
+
+                </thead>
+                @foreach ($movimientos as $mov)
+                <tr>
                     @if($mov->tipo=='FA')
                     <td>FACTURA</td>
-                   @elseif($mov->tipo=='AJ')
+                    @elseif($mov->tipo=='AJ')
                     <td>AJUSTE</td>
                     @elseif($mov->tipo=='NC')
                     <td>NOTA DE CREDITO</td>
                     @else
-                     <td>{{$mov->tipo}}</td>
+                    <td>{{$mov->tipo}}</td>
                     @endif
-					<td>{{ $mov->codigo}}</td>
-					<td>{{ $mov->fecha}}</td>					
-                    <td>{{ $mov->producto}}</td>	
+                    <td>{{ $mov->codigo}}</td>
+                    <td>{{ $mov->fecha}}</td>
+                    <td>{{ $mov->producto}}</td>
                     <td>{{ $mov->cantidad}}</td>
-                    <td>{{ $mov->precio}}</td>
-					<td>{{ $mov->costo}}</td>
-				</tr>		
-				@endforeach
-			</table>
-		</div>
-		{{$movimientos->render()}}
-		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-				<div class="form-group">
-						<select style="width:70px" name="formal" class="form-control" onchange="javascript:handleSelect(this)">
-								<option value="stock?pag=7"<?php 
+                    <td>$ {{ $mov->precio}}</td>
+                    <td>$ {{ $mov->costo}}</td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+        {{$movimientos->render()}}
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+            <div class="form-group">
+                <select style="width:70px" name="formal" class="form-control" onchange="javascript:handleSelect(this)">
+								<option value="stock?pag=7"<?php
 								if ($pag=='7') {
 									echo 'selected';
 								}?>>7</option>
-								<option value="stock?pag=15"<?php 
+								<option value="stock?pag=15"<?php
 								if ($pag=='15') {
 									echo 'selected';
 								}?>>15 </option>
-								<option value="stock?pag=25"<?php 
+								<option value="stock?pag=25"<?php
 								if ($pag=='25') {
 									echo 'selected';
 								}?>>25 </option>
-								<option value="stock?pag=50"<?php 
+								<option value="stock?pag=50"<?php
 								if ($pag=='50') {
 									echo 'selected';
 								}?>>50 </option>
-								<option value="stock?pag=100"<?php 
+								<option value="stock?pag=100"<?php
 								if ($pag=='100') {
 									echo 'selected';
 								}?>>100 </option>
 						</select>
-				</div>
-			</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
+
 @section ('script')
 <script type="text/javascript">
-	function handleSelect(elm){
+    function handleSelect(elm){
 		window.location = elm.value+"";
 	}
+
 </script>
