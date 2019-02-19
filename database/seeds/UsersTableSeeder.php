c5c6e33053d832a2b4de299c9db167428718d6e9
luzAmanda <luz_magcq94@hotmail.com>
2019-02-19 16:11:17 -0500

    validaciones

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -51,6 +51,58 @@ class UsersTableSeeder extends Seeder
             'usu_created_at' => Carbon::now(),
             'usu_updated_at' => Carbon::now()
         ]);
+        DB::table('inv_usuarios')->insert([
+            'usu_cedula' => '1004228282',
+            'usu_nombre' => 'Bodeguero',
+            'usu_apellido' => 'usuario',
+            'usu_fechaN' => '1992/1/3',
+            'usu_direccion' => 'Quito',
+            'usu_telefono' => '062345678',
+            'usu_celular' => '0934543234',
+            'usu_email' => 'bodeguero1@hotmail.com',
+            'usu_password' => bcrypt('12345678'),
+            'usu_created_at' => Carbon::now(),
+            'usu_updated_at' => Carbon::now()
+        ]);
+        DB::table('inv_usuarios')->insert([
+            'usu_cedula' => '1727465955',
+            'usu_nombre' => 'Bodeguero',
+            'usu_apellido' => 'usuario',
+            'usu_fechaN' => '1992/1/3',
+            'usu_direccion' => 'Quito',
+            'usu_telefono' => '062345678',
+            'usu_celular' => '0934543234',
+            'usu_email' => 'bodeguero2@hotmail.com',
+            'usu_password' => bcrypt('12345678'),
+            'usu_created_at' => Carbon::now(),
+            'usu_updated_at' => Carbon::now()
+        ]);
+        DB::table('inv_usuarios')->insert([
+            'usu_cedula' => '1003613724',
+            'usu_nombre' => 'Bodeguero',
+            'usu_apellido' => 'usuario',
+            'usu_fechaN' => '1992/1/3',
+            'usu_direccion' => 'Quito',
+            'usu_telefono' => '062345678',
+            'usu_celular' => '0934543234',
+            'usu_email' => 'bodeguero3@hotmail.com',
+            'usu_password' => bcrypt('12345678'),
+            'usu_created_at' => Carbon::now(),
+            'usu_updated_at' => Carbon::now()
+        ]);
+        DB::table('inv_usuarios')->insert([
+            'usu_cedula' => '8170850609',
+            'usu_nombre' => 'Bodeguero',
+            'usu_apellido' => 'usuario',
+            'usu_fechaN' => '1992/1/3',
+            'usu_direccion' => 'Quito',
+            'usu_telefono' => '062345678',
+            'usu_celular' => '0934543234',
+            'usu_email' => 'bodeguero4@hotmail.com',
+            'usu_password' => bcrypt('12345678'),
+            'usu_created_at' => Carbon::now(),
+            'usu_updated_at' => Carbon::now()
+        ]);
         DB::table('roles')->insert([
             'name' => 'root',
             'display_name' => 'Root',
@@ -87,5 +139,18 @@ class UsersTableSeeder extends Seeder
             'usu_id' => '3',
             'id' => '3',
         ]);
+        DB::table('role_user')->insert([
+            'usu_id' => '4',
+            'id' => '3',
+        ]);
+        DB::table('role_user')->insert([
+            'usu_id' => '5',
+            'id' => '2',
+        ]);
+        DB::table('role_user')->insert([
+            'usu_id' => '6',
+            'id' => '2',
+        ]);
+       
     }
 }
